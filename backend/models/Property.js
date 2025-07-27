63c42560e254a9e675d104f3bdf15077fd32d84d
Madhav Pruthi <madhav@Madhavs-MacBook-Air.local>
2025-07-27 18:48:25 +0530

    updated some code adminpnel and propertieslsitpage.

--- backend/models/Property.js
+++ backend/models/Property.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const PropertySchema = new mongoose.Schema({
   title: { type: String, required: true },
   location: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: String, required: true },
   description: { type: String },
   images: [String], // Array of image URLs
 });
